9ec369db895df0dfd45e594b2fbc8f214d11cedb
caio.furtado <caiohbfurtado@gmail.com>
2022-10-21 00:06:59 -0300

    add styled-components and nunito font

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+/* eslint-disable camelcase */
+import { StatusBar, Text } from 'react-native'
+import { ThemeProvider } from 'styled-components/native'
+import {
+  useFonts,
+  Nunito_400Regular,
+  Nunito_700Bold,
+} from '@expo-google-fonts/nunito'
+
+import theme from './src/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Text>Aplicação iniciada</Text> : null}
+    </ThemeProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
